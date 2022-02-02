7d429de8bc689bef2f11376349ce6f33bc5ed0a3
Xiaotian Yang <1977557517@qq.com>
2022-02-01 21:29:57 -0500

    ScoreCounter
    Finished ScoreCounter text object and it's script.

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    public Text scoreGT;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject scoreGo = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGo.GetComponent<Text>();
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
@@ -28,5 +32,9 @@ public class Basket : MonoBehaviour
         {
             Destroy(collidedWith);
         }
+
+        int score = int.Parse(scoreGT.text);
+        score += 100;
+        scoreGT.text = score.ToString();
     }
 }
