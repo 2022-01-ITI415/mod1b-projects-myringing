b8e825740603a87fd6e13dfb5c9dea0f127426b3
Xiaotian Yang <1977557517@qq.com>
2022-02-01 19:42:45 -0500

    AppleTree Movement
    Finished the coding about the movement of AppleTree, now it can be move to right and left and randomly change  the direction

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -7,7 +7,7 @@ public class AppleTree : MonoBehaviour
     public GameObject applePrefab;
     public float speed = 1f;
     public float leftAndRightEdge = 0.1f;
-    public float changeToChangeDirection = 0.1f;
+    public float chanceToChangeDirections = 0.1f;
     public float secondsBetweenAppleDrops = 1f;
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,26 @@ public class AppleTree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
     }
 }
