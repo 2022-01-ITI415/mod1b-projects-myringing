1c9a85dfd963f9e4385385f626feec92130f3a45
Xiaotian Yang <xy202@scarletmail.rutgers.edu>
2022-02-10 19:56:14 -0500

    More about FollowCam

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour
 {
     static public FollowCam S;
+    public float easing = 0.05f;
+    public Vector2 minXY;
     public bool _____________________________;
     public GameObject poi;
     public float camZ;
@@ -25,7 +27,11 @@ public class FollowCam : MonoBehaviour
     {
         if (poi == null) return;
         Vector3 destination = poi.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
     }
 }
