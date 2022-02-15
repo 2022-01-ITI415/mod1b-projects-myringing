15d3f4327ba39371ed7f029cf7d8eec1c9d27e93
Xiaotian Yang <1977557517@qq.com>
2022-02-14 23:42:45 -0500

    Camera Follow
    Finished the scripts about Camera Follow, then add walls to the playground.

--- Assets/03-Prototype1/Scripts/Cam.cs
+++ Assets/03-Prototype1/Scripts/Cam.cs
@@ -6,6 +6,9 @@ public class Cam : MonoBehaviour
 {
     public float camY;
     public GameObject player;
+    public Vector3 mousePos;
+    public Vector3 destination;
+    public float easing = 0.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +18,16 @@ public class Cam : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        mousePos = GetMouse.S.mousePos3D;
+        mousePos.y = camY;
         Vector3 pos = player.transform.position;
         pos.y = camY;
-        this.transform.position = pos;
+        destination = (pos + mousePos) / 2;
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        transform.position = destination;
+        //if (Mathf.Abs(destination.x) <= 5 && Mathf.Abs(destination.z) <= 5)
+        //{            
+            //this.GetComponent<Camera>().orthographicSize = Mathf.Max(Mathf.Abs(destination.x), Mathf.Abs(destination.z)) / 2 + 5;
+        //}
     }
 }
